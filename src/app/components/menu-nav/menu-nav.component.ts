0e9401e0abccae8c8697935768ba8695ada955a4
felpsnaitor <felpsnaitor@gmail.com>
2023-12-19 00:26:47 -0300

    conectando ao github

---
+++ src/app/components/menu-nav/menu-nav.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-nav',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu-nav.component.html',
+  styleUrl: './menu-nav.component.css'
+})
+export class MenuNavComponent {
+
+}
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {MenuTitleComponent} from '../../components/menu-title/menu-title.component'
+import {BigCardComponent} from '../../components/big-card/big-card.component'
+import {MenuNavComponent} from '../../components/menu-nav/menu-nav.component'
+import {SmallCardComponent} from '../../components/small-card/small-card.component'
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [MenuTitleComponent, BigCardComponent, MenuNavComponent, SmallCardComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
